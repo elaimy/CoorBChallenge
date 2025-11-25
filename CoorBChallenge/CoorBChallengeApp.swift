adbce9a3d863df6efff5020637314ce897b98f7f
Ahmed Elelaimy <ahmed.elaimy@hotmail.com>
2025-11-25 10:39:18 +0100

    Initial project setup

---
+++ CoorBChallenge/CoorBChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoorBChallengeApp.swift
+//  CoorBChallenge
+//
+//  Created by Ahmed Elelaimy on 25/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoorBChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
