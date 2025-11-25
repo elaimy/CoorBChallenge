d682ffa977d5e4d561e9f18db44449d473a77495
Ahmed Elelaimy <ahmed.elaimy@hotmail.com>
2025-11-25 11:10:39 +0100

    Set up initial project structure

---
+++ CoorBChallenge/Features/CountryList/Views/CountryListView.swift
@@ -0,0 +1,18 @@
+//
+//  CountryListView.swift
+//  CoorBChallenge
+//
+//  Created by Ahmed Elelaimy on 25/11/2025.
+//
+
+import SwiftUI
+
+struct CountryListView: View {
+    var body: some View {
+        Text("Country List")
+    }
+}
+
+#Preview {
+    CountryListView()
+}
